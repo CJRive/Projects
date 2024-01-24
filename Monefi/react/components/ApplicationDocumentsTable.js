070932de1089d467d92c698649e6e860450d0af0
Carlos Rivera <130215883+CJRive@users.noreply.github.com>
2024-01-24 02:04:51 -0800

    Create ApplicationDocumentsTable.js

---
+++ Monefi/react/components/ApplicationDocumentsTable.js
@@ -0,0 +1,61 @@
+import React from "react";
+import { Card } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import PropTypes from "prop-types";
+import DocumentsTable from "./borrowerinfo/VerificationsTable";
+
+function ApplicationDocumentsTable({ verifications }) {
+  const documentsColumns = React.useMemo(
+    () => [
+      {
+        Header: <div className="borrower-table-text ml-2">Type</div>,
+        accessor: "verificationType",
+      },
+      {
+        Header: <div className="borrower-table-text">Status</div>,
+        accessor: "isVerified",
+      },
+      {
+        Header: <div className="borrower-table-text">Results</div>,
+        accessor: "isConfirmed",
+      },
+      {
+        Header: <div className="borrower-table-text">Verified On</div>,
+        accessor: "verifiedOn",
+      },
+      {
+        Header: <div className="borrower-table-text">Comments</div>,
+        accessor: "comment",
+      },
+    ],
+    []
+  );
+  return (
+    <Card className="h-100">
+      <Card.Header className="border-0">
+        <div className="justify-content-between align-center d-flex">
+          <h1 className="m-2">Application Documents</h1>
+          <Link to={`/borrower/verification`}>
+            <div className="btn btn-sm btn-outline-white text-end">View More</div>
+          </Link>
+        </div>
+      </Card.Header>
+      <div className="borrower-table">
+        <DocumentsTable columns={documentsColumns} data={verifications} />
+      </div>
+    </Card>
+  );
+}
+
+ApplicationDocumentsTable.propTypes = {
+  verifications: PropTypes.arrayOf(
+    PropTypes.shape({
+      verificationType: PropTypes.shape({}),
+      isVerified: PropTypes.shape({}),
+      isConfirmed: PropTypes.shape({}),
+      verifiedOn: PropTypes.shape({}),
+    })
+  ),
+};
+
+export default React.memo(ApplicationDocumentsTable);
